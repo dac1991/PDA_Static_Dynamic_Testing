0bc791ea82f3de3ec18b0785b443d93963697b53
Dominic Andrew <71727000+dac1991@users.noreply.github.com>
2021-04-23 10:01:38 +0100

    PDA Exercise complete

---
+++ PDA Static & Dynamic Testing/part_2_code/specs/card_game_tests.py
@@ -0,0 +1,22 @@
+import unittest
+from src.card import Card
+from src.card_game import CardGame
+
+class TestCardGame(unittest.TestCase):
+    
+    def setUp(self):
+        self.card1 = Card("Hearts", 4)
+        self.card2 = Card("Clubs", 8)
+        self.card_game = CardGame()
+      
+
+    def test_check_for_ace(self):
+        self.assertEqual(False, self.card_game.check_for_ace(self.card1))
+
+    def test_highest_card(self):
+        self.assertEqual(self.card2, self.card_game.highest_card(self.card1, self.card2))
+
+
+    def test_cards_total(self):
+        cards = [self.card1, self.card2]
+        self.assertEqual("You have a total of 12", self.card_game.cards_total(cards))
